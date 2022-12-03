b91c60401c600241dc93823bb40045f725934545
Rustam <pkheshkhov0707@mail.ru>
2022-12-03 16:53:21 +0300

    третья задача

---
+++ задача 3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+
+int Getnumber(string message)
+
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+       if (int.TryParse(Console.ReadLine(), out result) && result>0)
+       {
+        isCorrect = true;
+       }
+       else
+       {
+        Console.WriteLine("Ввели не натуральное число");
+       }
+    }
+return result;
+}
+
+int number = Getnumber("введите число:");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i}, ");
+
+};
